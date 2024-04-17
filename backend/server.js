4de28d5d5f9cd91892d9994717f8220e9c002526
hetalgada15 <114257026+hetalgada15@users.noreply.github.com>
2024-04-17 13:15:47 -0400

    server updating js

--- backend/server.js
+++ backend/server.js
@@ -10,7 +10,15 @@ import roomRoutes from './routes/roomsRoutes.js'
 import userRoutes from './routes/userRoutes.js'
 import packageesRoutes from './routes/packageRoutes.js'
 import studioRoutes from './routes/studioRoutes.js'
+import conferenceRoutes from './routes/conferenceRoutes.js'
+import conInsideRoutes from './routes/conInsideRoute.js'
+import uploadRoutes from './routes/uploadRoutes.js'
 
+import bookingRoutes from './routes/bookingRoutes.js'
+
+import skincareRoutes from './routes/skincareRoutes.js'
+import orderRoutes from './routes/orderRoutes.js'
+import tableBookingRoutes from './routes/tableBookingRoutes.js'
 
 dotenv.config()
 
@@ -28,15 +36,15 @@ if (process.env.NODE_ENV === 'development') {
 app.use(cors())
 app.use(express.json())
 
-
 //calling routes
 app.use('/api/rooms', roomRoutes)
 app.use('/api/users', userRoutes)
 app.use('/api/packagees', packageesRoutes)
 app.use('/api/studios', studioRoutes)
-
-
-
+app.use('/api/conference', conferenceRoutes)
+app.use('/api/conInside', conInsideRoutes)
+app.use('/api/uploads', uploadRoutes)
+app.use('/api/orders', orderRoutes)
 
 const __dirname = path.resolve()
 app.use('/images', express.static(path.join(__dirname, '/images')))
