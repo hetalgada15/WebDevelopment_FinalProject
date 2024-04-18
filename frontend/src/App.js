16b5c7bbd7ee27bc03d85ff3afec0db5c8ce66cd
Your Name <youremail@example.com>
2024-04-18 00:10:03 -0400

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -20,6 +20,11 @@ import AllSkincares from './screens/SkincaresScreen/skincaresScreen';
 import CreateRoomScreen from './screens/CreateRoomScreen.js/createRoomScreen';
 import SkincareUpdateScreen from './screens/SkincareUpdateScreen/SkincareUpdateScreen';
 import PackageUpdateScreen from './screens/PackageUpdateScreen/PackageUpdateScreen';
+import OrderListScreen from './screens/OrderListScreen/orderListScreen';
+import SkincareDetail from './screens/SkincareDetailScreen/skincareDetailScreen'
+import CartScreen from './screens/CartScreen/cartScreen'
+import ShippingScreen from './screens/ShippingScreen/shippingScreen'
+import PaymentScreen from './screens/PaymentScreen/payementScreen'
 
 
 const App = () => {
@@ -33,7 +38,7 @@ const App = () => {
         <Route path="/profile" component={UserProfile} />
         <Route path="/rooms" component={RoomsScreen} />
         <Route path="/studioInsert" component={StudioInsertScreen} />
-        <Route path="/conferenceInsert" component={conferenceInsert} />       
+        <Route path="/conferenceInsert" component={conferenceInsert} />
         <Route path="/packageList" component={PackageList} />
         <Route path="/conference" component={ConferenceScreen} />
         <Route path="/packages" component={PackageScreen} />
@@ -46,7 +51,14 @@ const App = () => {
         <Route path="/skincares" component={AllSkincares} />
         <Route path="/admin/createRoom" component={CreateRoomScreen} />
         <Route path="/skincareUpdate/:id" component={SkincareUpdateScreen} />
+        <Route path="/packagemanagement/update/:id" component={PackageUpdateScreen}/>
         <Route path="/updatePackage/:id" component={PackageUpdateScreen}/>
+        <Route path="/updatePackage/:id" component={PackageUpdateScreen}/>
+        <Route path='/orderList' component={OrderListScreen} />
+        <Route path="/skincare/:id" component={SkincareDetail} />
+        <Route path='/cart/:id?' component={CartScreen} />
+        <Route path='/shipping' component={ShippingScreen} />
+        <Route path='/payment' component={PaymentScreen} />
       </div>
     </Router>
   );
